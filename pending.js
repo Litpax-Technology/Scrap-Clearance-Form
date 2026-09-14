01060be354ae76b9ccf510dcefc9fa967c1a2906
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-14 13:57:08 +0530

    Update pending.js

--- pending.js
+++ pending.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbz5iMArcJmDdpaCjn3RwcgGSSlHMypD09Y87SGXAtP2HG5GNqdBd4ztkextufwUOczn1Q/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzxfDJVsVp7gB_fzLqgCvp1I8H7kgKQpeYD5g2eDIhROzGTtGLQewlH1olxYevWaLrAvQ/exec";
 
 const clearanceSelect = document.getElementById("clearanceSelect");
 const receiveInput = document.getElementById("receiveInput");
