20db2dc51b5143171aa03e91938d4ca5841c8f97
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-24 11:56:52 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbweeEe69SNbee6slz6iGoS2bHKSCvkSSBADpPGlYamfMo8VfGqhfSlSipY7C7mGIFZ_ag/exec",
+  SCRAP_TYPES: ["Metal Scrap", "Plastic Scrap", "E-Waste", "Mixed Scrap", "Other"],
+  PAY_MODES: ["Cash", "UPI", "Bank Transfer"]
+};
